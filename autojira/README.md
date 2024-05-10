6a9b23364cf6e0d9d897d542d7e30452dbf95675
DanyalMh <89704186+Rustixir@users.noreply.github.com>
2024-05-10 14:44:55 +0330

    Delete autojira directory

--- autojira/README.md
+++
@@ -1,1 +0,0 @@
-Tell the AI ​​your problem and it will identify it as a feature or bug and record that problem in jira with great description.
--- autojira/infrastructure/openai.go
+++
@@ -1,7 +0,0 @@
-package infrastructure
-
-import "github.com/sashabaranov/go-openai"
-
-func NewOpenAI(token string) *openai.Client {
-	return openai.NewClient(token)
-}
